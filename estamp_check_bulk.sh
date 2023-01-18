e247d49456951c0bca4de4b310146563687f8a4b
abdularshad06 <abdularshad390@gmail.com>
2023-01-18 12:25:44 +0530

    Create estamp_check_bulk.sh

---
+++ estamp_check_bulk.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/bash
+##Created By Arshad
+dest=""
+src=""
+filename="test.txt"
+while read -r line; do
+    name="$line"
+    if [[ $name =~ /shared ]]
+    then
+        dest=$name
+    elif [[ $name =~ /ftpvolume ]]
+    then
+        src=$name
+        echo "scp $src/EStamp.PDF oracle@10.125.243.5:$dest/EStamp.PDF"
+        #scp "$src/EStamp.PDF" "oracle@10.125.243.5:$dest/EStamp.PDF"
+    fi
+    #sleep 1
+done< "$filename"
